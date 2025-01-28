fb8db76356edfd5da22c424cfdb110a8ced537ad
Chamith <chamith.eos@gmail.com>
2025-01-28 17:42:23 +0530

    hello world, server a html page, print path

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
-
-app.get('/hello', (req,res) => {
-    res.send("Hello Word");
+const http = require('http');
+const fs = require("fs").promises;
+const server = http.createServer((req,res) =>{
+    if (req.url.startsWith("/hello")) {
+        const pathParts = req.url.split("/hello/");
+        const dynamicPart = pathParts[1] || "";
+        res.StatusCode = 200
+        res.setHeader("Content-Type","text/json")
+        res.end(JSON.stringify({name:"hello-world",
+            "path":dynamicPart}))
+        console.log(req.url);
+    }
+    if (req.url.startsWith("/html")){
+        fs.readFile(__dirname+"/sample.html")
+            .then(content =>{
+                res.setHeader("Content-Type","text/html");
+                res.writeHead(200);
+                res.end(content);
+            })
+    }
+    
 });
-
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-  });
+const port = 3000
+server.listen(port, ()=>{
+    console.log(`Server Listening on port ${port}`)
+})
